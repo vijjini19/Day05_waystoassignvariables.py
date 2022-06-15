f6362982e6be7013a9e6505e4b0a16d5a2d48315
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-15 10:32:04 +0530

    Add files via upload

---
+++ ways_to_assign_variables_Day05.py
@@ -0,0 +1,25 @@
+"""ways to assign variables"""
+# a=1
+# b=2
+# c=3
+# print(a)
+# print(b)
+# print(c)
+# print(d) #name error
+# print(a,b,c)
+
+# a=1 ; b=2 ; c=3
+# print(a,b,c)
+# print(a);print(b);print(c)
+
+# a,b,c = 89,45 #value error
+# a,b,c = 67,897,57,4 #value error
+
+# a=c=7;b=8
+# print(a,b,c)
+# print(a,c,c)
+# print(c,c,c)
+
+# n=1,2,3,4,5
+# print(n,type(n))
+
